ee92cbe97e8c0ac23379af736cba674ca33bb4fd
egex77777 <111453781+egex77777@users.noreply.github.com>
2023-11-26 11:41:10 +0300

    Silahçı Dükkanı adlı varlık eklendi!
    çok yakında oyun içine eklenecek

--- donator_shop/lua/entities/donatormarket/shared.lua
+++ donator_shop/lua/entities/donatormarket/shared.lua
@@ -2,19 +2,48 @@ ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
 
 ENT.PrintName = "Donator Marketi"
-ENT.Category = "Reccpe Entities"
+ENT.Category = "SavioR-RScripting"
 
 ENT.Spawnable = true
 
 DONATORMARKET = DONATORMARKET or {}
 DONATORMARKET.VIPGruplar = {
-    ["donator"] = true,
+    ["silahci_acces"] = true,
+    ["silahci_acces-vip"] = true,
 }
 DONATORMARKET.Items = {
     {
         isim = "Bayonet Fade", // this is name of the category
         fiyat = 65000, // this is price of category
         silahID = "csgo_bayonet_fade", // This is Weapon ID
+        donator = true // if u wanna turn off the accessable of donators u can type the 'false'
+    },
+
+     {
+        isim = "SVK", // this is name of the category
+        fiyat = 250000, // this is price of category
+        silahID = "wf_wpn_sr02", // This is Weapon ID
+        donator = true // if u wanna turn off the accessable of donators u can type the 'false'
+    },
+    
+    {
+        isim = "AR-GAR", // this is name of the category
+        fiyat = 125000, // this is price of category
+        silahID = "wf_wpn_ar23", // This is Weapon ID
+        donator = true // if u wanna turn off the accessable of donators u can type the 'false'
+    },
+    
+    {
+        isim = "RGX160", // this is name of the category
+        fiyat = 125000, // this is price of category
+        silahID = "wf_wpn_ar27", // This is Weapon ID
+        donator = true // if u wanna turn off the accessable of donators u can type the 'false'
+    },
+    
+    {
+        isim = "HP Tabanca", // this is name of the category
+        fiyat = 25000, // this is price of category
+        silahID = "wf_wpn_pt05", // This is Weapon ID
         donator = true // if u wanna turn off the accessable of donators u can type the 'false'
     },
 }
